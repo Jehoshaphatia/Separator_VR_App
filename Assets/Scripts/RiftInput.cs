c3c1b22455006f484cb9a996de38e7ff389689c8
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2019-03-14 16:11:12 +0100

    Feat: add assement component

--- Assets/Scripts/RiftInput.cs
+++ Assets/Scripts/RiftInput.cs
@@ -4,36 +4,38 @@ using UnityEngine;
 using Valve.VR;
 
 public class RiftInput : MonoBehaviour {
-    [SteamVR_DefaultAction("Interact With UI")]
-    public SteamVR_Action_Single interactWithUI;
+    //[SteamVR_DefaultAction("Interact With UI")]
+    //public SteamVR_Action_Single interactWithUI;
 
     
     public GameObject componentDetailsCanvas; // Assign in inspector
     public GameObject componentLabels;
     private bool isShowing;
+    //public AudioSource componentVoiceOver;
     // Update is called once per frame
     void Update ()
     {
         if (SteamVR_Input._default.inActions.Teleport.GetStateDown(SteamVR_Input_Sources.Any))
         {
-            Debug.Log("Teleport Button down");
+           //Event code goes here
         }
         if (SteamVR_Input._default.inActions.GrabGrip.GetStateUp(SteamVR_Input_Sources.Any))
         {
-            Debug.Log("GrabGrip Button down");
+            //Event code goes here
         }
 
-        float trigerValue = interactWithUI.GetAxis(SteamVR_Input_Sources.Any);
+        //float trigerValue = interactWithUI.GetAxis(SteamVR_Input_Sources.Any);
 
-        if (trigerValue > 0.0f)
-        {
-            Debug.Log("Trigger button pulled");
-        }
+        //if (trigerValue > 0.0f)
+        //{
+        //    //Event code goes here
+        //}
     }
 
     public void ShowCanvas()
     {
         //Canvas gameobject enable
+        //componentVoiceOver.Play();
         isShowing = !isShowing;
         componentDetailsCanvas.SetActive(isShowing);
         componentLabels.SetActive(isShowing);
